588aca5827207174d206005b87691db4fa8048fd
Bruna <228819712+brunadpaulasilva@users.noreply.github.com>
2026-01-31 14:45:07 -0300

    Add files via upload

---
+++ Aula 03/calculadora.py
@@ -0,0 +1,41 @@
+
+# n1 = float( input('Digite um número') )
+# n2 = float( input('Digite um número') )
+
+# soma  =  n1 + n2
+# print(soma)
+
+print('Calculadora Python')
+
+# não pode utilizar condicionais
+# Utiliza sinais aritméticos
+# print / input()
+# concatenação
+# 4 operações básicas
+
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite um número: '))
+
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+
+print('O Resultado da soma entre n1 e n2 é: ',soma)
+print('O Resultado da subtração entre n1 e n2 é: ',subtracao)
+print('O Resultado da multiplicação entre n1 e n2 é: ',multiplicacao)
+print('O Resultado da divisão entre n1 e n2 é: ',divisao)
+
+print('Calculadora Python')
+
+n1 = float(input('= '))
+n2 = float(input('= '))      
+
+# f_string
+
+print(f'''
+Soma - {n1 +  n2}
+Substração -  {n1  - n2}
+Multiplicação -  {n1 * n2}
+Divisão  -  {n1 / n2}
+''')
